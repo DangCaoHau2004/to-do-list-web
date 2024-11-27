924282530a08c10915d8b767b654ef3d226296c0
Dang Cao Hau <caohauhau9@gmail.com>
2024-11-27 14:20:37 +0700

    sửa .env và cấu trúc sql

--- index.js
+++ index.js
@@ -7,28 +7,41 @@ const app = express();
 dotenv.config();
 
 const port = 3000;
+const HOST = process.env.HOST;
 const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
-
+const DATABASE = process.env.DATABASE;
+const PORT = process.env.PORT;
+// thiết lập database
 const db = new pg.Client({
   user: USER,
-  host: "localhost",
-  database: "postgres",
+  host: HOST,
+  database: DATABASE,
   password: PASSWORD,
-  port: 5432,
+  port: PORT,
 });
-db.connect();
+
+// kết nối database
+db.connect()
+  .then(() => console.log("Connected to database successfully"))
+  .catch((err) => {
+    console.log("Error connecting to database: ", err);
+    process.exit(1);
+  });
+
+//middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 async function checkAllItems() {
+  await db.query(
+    "CREATE TABLE IF NOT EXISTS items (id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL);"
+  );
   const result = await db.query("SELECT * FROM items ORDER BY id");
   return result.rows;
 }
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+let items = [];
 
+// homepage
 app.get("/", async (req, res) => {
   items = await checkAllItems();
   res.render("index.ejs", {
@@ -36,19 +49,20 @@ app.get("/", async (req, res) => {
     listItems: items,
   });
 });
-
+// post add
 app.post("/add", async (req, res) => {
   const item = req.body.newItem;
   await db.query("INSERT INTO items(title) VALUES($1)", [item]);
   res.redirect("/");
 });
-
+// post edit
 app.post("/edit", async (req, res) => {
   const id = req.body.updatedItemId;
   const title = req.body.updatedItemTitle;
   await db.query("UPDATE items SET title = $1 WHERE id = $2", [title, id]);
   res.redirect("/");
 });
+//post delete
 app.post("/delete", async (req, res) => {
   const id = req.body.deleteItemId;
   await db.query("DELETE FROM items WHERE id = $1", [id]);
